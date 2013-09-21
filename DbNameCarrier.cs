921ff9f8040688cefd8250ee69b2c13e85b4381b
adarapata <efb.youdie@gmail.com>
2013-09-21 11:13:16 +0900

    外部からDB名を設定できるように変更

---
+++ DbNameCarrier.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DbNameCarrier : MonoBehaviour {
+
+    public string dbname = "database_name.db";
+    void Awake()
+    {
+    	SqliteDatabase.SetDatabaseName(dbname);
+    }
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
