f6a2823e2c92d33a579c125201774166d4983240
adarapata <efb.youdie@gmail.com>
2014-02-12 01:45:19 +0900

    indent...

--- Sample/NattoSample.cs
+++ Sample/NattoSample.cs
@@ -2,18 +2,19 @@
 using System.Collections;
 using Natto;
 
-public class NattoSample : MonoBehaviour {
+public class NattoSample : MonoBehaviour
+{
+    // Use this for initialization
+    void Start ()
+    {
+        SampleDao dao = SampleDao.Find (n => n.param_string == "okame");
+        print (dao.param_string);
 
-  	// Use this for initialization
-	  void Start () {
-       SampleDao dao = SampleDao.Find(n => n.param_string == "okame");
-       print(dao.param_string);
+        SampleDao.Where (n => n.param_int > 60).ForEach (n => print (n.param_string));
+    }
 
-       SampleDao.Where(n => n.param_int > 60).ForEach(n => print(n.param_string));
-	  }
-	
-	  // Update is called once per frame
-	  void Update () {
-	
-	  }
+    // Update is called once per frame
+    void Update ()
+    {
+    }
 }
