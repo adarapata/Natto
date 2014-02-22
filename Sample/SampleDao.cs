cda1467a8988524ee5a66a8ba21fe4209a3134d3
adarapata <efb.youdie@gmail.com>
2014-02-22 14:19:46 +0900

    サンプルちょっと修正

--- Sample/SampleDao.cs
+++ Sample/SampleDao.cs
@@ -11,4 +11,6 @@ public class SampleDao : ActiveRecord<SampleDao>
     public int param_int { get { return GetInt("param_int"); } set { this["param_int"] = value; } }
     public string param_string { get { return GetString("param_string"); } set { this["param_string"] = value; } }
     public bool param_bool { get { return GetBool("param_bool"); } set { this["param_bool"] = value; } }
+    public int? param_int_null { get { return GetIntOrNull("param_int_null"); } set { this["param_int_null"] = value; } }
+    public string param_string_null { get { return GetString("param_string_null"); } set { this["param_string_null"] = value; } }
 }
