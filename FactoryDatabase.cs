d15ad1e638644921e71c9752238599e608ade8b5
adarapata <efb.youdie@gmail.com>
2014-03-29 15:56:03 +0900

    設定したDBによって返す物を変えるようにした

--- FactoryDatabase.cs
+++ FactoryDatabase.cs
@@ -4,9 +4,16 @@ using Natto.Interface;
 
 namespace Natto {
     public class FactoryDatabase {
+        static public DatabaseType databaseType { get; set; }
         static public IDatabase CreateDatabase()
         {
-            return SqliteDatabase.instance;
+            switch(databaseType)
+            {
+                case DatabaseType.SQLite: return SqliteDatabase.instance;
+                case DatabaseType.MySQL: return MysqlDatabase.instance;
+            }
+            UnityEngine.Debug.Log("No Setting Database Type");
+            return null;
         }
     }
 }
