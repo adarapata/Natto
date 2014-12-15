4adbaad3e89d033f79094a192d33f5a20f58db69
adarapata <efb.youdie@gmail.com>
2014-12-15 23:50:12 +0900

    Database情報を持つクラスをScriptableObjectに

---
+++ DatabaseConfig.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Natto.Database;
+
+namespace Natto {
+	public class DatabaseConfig : ScriptableObject {
+
+        public DatabaseType databaseType;
+        public string dbname = "database_name.db";
+        public string hostName,database,userName,port,password;
+
+		void OnEnable()
+		{
+			switch(databaseType)
+			{
+				case DatabaseType.SQLite: 
+					SqliteDatabase.SetDatabaseName(dbname);
+					break;
+				case DatabaseType.MySQL:
+					MysqlDatabase.SetDatabaseName(hostName,database,userName,port,password);
+					break;
+			}
+			FactoryDatabase.databaseType = databaseType;
+		}
+    }
+}
